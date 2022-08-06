556d1940bf534a3cb9c75f1774d5133f1a7780c1
Jay Jackson <jj@jj.is-a.dev>
2022-08-06 17:09:52 -0400

    booting

---
+++ qemu/src/main.rs
@@ -0,0 +1,111 @@
+#![feature(stmt_expr_attributes)]
+
+use std::{
+    path::Path,
+    process::{Command, Stdio},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Building the kernel...");
+    let root = env!("CARGO_MANIFEST_DIR");
+    let root = Path::new(&root).parent().unwrap();
+    Command::new(env!("CARGO"))
+        .args(&[
+            "build",
+            #[cfg(not(debug_assertions))]
+            "--release",
+            "--package=kernel",
+            "--target=x86_64-unknown-uefi",
+            "-Zbuild-std=core,compiler_builtins,alloc",
+            "-Zbuild-std-features=compiler-builtins-mem",
+        ])
+        .current_dir(root)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()?
+        .wait()?;
+
+    let target_dir = root.join("target");
+
+    let efi_dir = target_dir.join("efi");
+    let boot_dir = efi_dir.join("EFI").join("Boot");
+    std::fs::create_dir_all(&boot_dir)?;
+
+    std::fs::copy(
+        target_dir
+            .join("x86_64-unknown-uefi")
+            .join(if cfg!(debug_assertions) {
+                "debug"
+            } else {
+                "release"
+            })
+            .join("kernel.efi"),
+        boot_dir.join("BOOTX64.EFI"),
+    )?;
+
+    if let Some(arg) = std::env::args().nth(1) && arg == "--no-run" {
+        println!("Not launching qemu...");
+        return Ok(());
+    }
+
+    println!("Launching qemu...");
+
+    let qemu_args = &[
+        "-nodefaults",
+        "-machine",
+        "q35",
+        "-smp",
+        "4",
+        "-m",
+        "256M",
+        "-device",
+        "isa-debug-exit,iobase=0xf4,iosize=0x04",
+        "-bios",
+        concat!(env!("OUT_DIR"), "/OVMF.fd"),
+        "-drive",
+        "file=fat:rw:target/efi,format=raw",
+        "-vga",
+        "std",
+        "-serial",
+        "stdio",
+    ];
+
+    println!("qemu-system-x86_64 {}", qemu_args.join(" "));
+
+    Command::new("qemu-system-x86_64")
+        .args(qemu_args)
+        .current_dir(root)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()?
+        .wait()?;
+
+    Ok(())
+}
+const _: &[&str] = &[
+    "qemu-system-x86_64",
+    "-nodefaults",
+    "-device",
+    "virtio-rng-pci",
+    "-machine",
+    "q35",
+    "-smp",
+    "4",
+    "-m",
+    "256M",
+    "-device",
+    "isa-debug-exit,iobase=0xf4,iosize=0x04",
+    "-drive",
+    "if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd",
+    "-drive",
+    "if=pflash,format=raw,readonly=off,file=/tmp/.tmpvUxKx6/ovmf_vars",
+    "-drive",
+    "format=raw,file=fat:rw:target/x86_64-unknown-uefi/release/esp",
+    "-vga",
+    "std",
+    "-drive",
+    "format=raw,file=/tmp/.tmpvUxKx6/test_disk.fat.img",
+    "-serial",
+    "stdio",
+    "-nic",
+];
