978e48d87889a80dabd58fb44e8a09b1016598d8
Jay Jackson <jj@jj.is-a.dev>
2022-09-16 22:08:06 -0400

    wip graphics

--- kernel/src/graphical.rs
+++ kernel/src/graphical.rs
@@ -1,14 +1,54 @@
-use bootloader_api::info::{FrameBuffer, PixelFormat};
+use core::{cell::UnsafeCell, convert::Infallible};
 
 use crate::late_init::LateInit;
+use bootloader_api::info::{FrameBuffer as InnerFrameBuffer, PixelFormat};
+use embedded_graphics::{
+    pixelcolor::Rgb888,
+    prelude::{DrawTarget, OriginDimensions, Size},
+};
 
-pub static FRAMEBUFFER: LateInit<FrameBuffer> = LateInit::new();
+pub struct FrameBufferWriter<'a>(&'a mut InnerFrameBuffer);
+impl DrawTarget for FrameBufferWriter<'_> {
+    type Color = Rgb888;
+    type Error = Infallible;
 
-pub fn init_framebuffer(fb: Option<FrameBuffer>) {
+    fn draw_iter<I>(&mut self, _pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = embedded_graphics::Pixel<Self::Color>>,
+    {
+        todo!();
+    }
+}
+impl OriginDimensions for FrameBufferWriter<'_> {
+    fn size(&self) -> Size {
+        Size::new(self.0.info().width as u32, self.0.info().height as u32)
+    }
+}
+
+pub struct FrameBuffer(LateInit<UnsafeCell<InnerFrameBuffer>>);
+unsafe impl Sync for FrameBuffer {}
+unsafe impl Send for FrameBuffer {}
+impl FrameBuffer {
+    pub const fn new() -> Self {
+        Self(LateInit::new())
+    }
+    pub fn init(&self, framebuffer: InnerFrameBuffer) {
+        self.0.init(UnsafeCell::new(framebuffer))
+    }
+    pub fn get(&self) -> FrameBufferWriter<'_> {
+        FrameBufferWriter(unsafe { &mut *self.0.get().get() })
+    }
+}
+
+pub static FRAMEBUFFER: FrameBuffer = FrameBuffer::new();
+
+pub fn init_framebuffer(fb: Option<InnerFrameBuffer>) {
     let mut fb = fb.expect("no framebuffer");
     fb.buffer_mut().fill(0);
+
     if matches!(fb.info().pixel_format, PixelFormat::Unknown { .. }) {
         panic!("framebuffer has unknown pixel format");
     }
+
     FRAMEBUFFER.init(fb);
 }
